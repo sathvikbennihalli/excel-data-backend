17e8b814adca86623c01ae9774c67831fed5b00b
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-04-03 13:30:42 +0530

    committing backend

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "193.203.184.81",
+  user: "u540517340_zepto",
+  password: ">esD[9d^U",
+  database: "u540517340_zepto",
+});
+
+app.get("/", (re, res) => {
+  return res.json("From backend side");
+});
+
+app.get("/agents", (req, res) => {
+  const sql = "SELECT * FROM agents";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.listen(3001, () => {
+  console.log("listening");
+});
